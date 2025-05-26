6fcb620335448a2c9b2165fb4b20751ae9229c4b
Van Chi Hieu <vanchihieu3@gmail.com>
2025-05-26 13:50:04 +0700

    Implement Boyer-Moore Voting Algorithm for finding the majority element in an array

---
+++ Array/MajorityElement.js
@@ -0,0 +1,17 @@
+var majorityElement = function (nums) {
+  let candidate = nums[0];
+  let count = 1;
+
+  for (let i = 1; i < nums.length; i++) {
+    if (count === 0) {
+      candidate = nums[i];
+      count = 1;
+    } else if (nums[i] === candidate) {
+      count++;
+    } else {
+      count--;
+    }
+  }
+
+  return candidate;
+};
